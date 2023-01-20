c882caa240b90fe30ba68a2dacd0581445d685ac
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2023-01-19 19:34:21 -0600

    fix: remove leftover tailwind classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
       <Box p='2'>
         <Flex minWidth='max-content' alignItems='center' gap='2'>
           <Icon boxSize={6}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </Icon>
