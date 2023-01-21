a6aef00f258985840779f2b5097ce645eb17b40e
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2023-01-21 16:40:12 -0600

    feat: add an unauthed view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react';
 import { type NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import Navbar from '../components/Navbar';
@@ -14,7 +15,19 @@ const Home: NextPage = () => {
       <Navbar />
       <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="flex flex-col items-center gap-2">{todos && <TodoList todos={todos} />}</div>
+          <div className="flex flex-col items-center gap-2">
+            {todos && <TodoList todos={todos} />}
+            {!sessionData && (
+              <Card>
+                <CardHeader>
+                  <Heading>Welcome to Raise Todo!</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>Please sign in to get started.</Text>
+                </CardBody>
+              </Card>
+            )}
+          </div>
         </div>
       </main>
     </div>
